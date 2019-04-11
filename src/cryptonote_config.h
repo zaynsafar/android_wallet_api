14bac09a680c80e3399c3885f124e104503938e5
scilicet64 <scilicet64@github.com>
2019-04-11 16:15:47 +0200

    fix dynamic fee

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -89,11 +89,11 @@ static_assert(STAKING_PORTIONS % 3 == 0, "Use a multiple of three, so that it di
 
 #define FEE_PER_KB_OLD                                  ((uint64_t)100000000) // pow(10, 8)
 #define FEE_PER_KB                                      ((uint64_t)2000000) // 2 * pow(10, 6)
-#define FEE_PER_BYTE                                    ((uint64_t)300)
+#define FEE_PER_BYTE                                    ((uint64_t)30000000)
 #define DYNAMIC_FEE_PER_KB_BASE_FEE                     ((uint64_t)2000000) // 2 * pow(10,6)
 #define DYNAMIC_FEE_PER_KB_BASE_BLOCK_REWARD            ((uint64_t)10000000000) // 10 * pow(10,9)
 #define DYNAMIC_FEE_PER_KB_BASE_FEE_V5                  ((uint64_t)20000000 * (uint64_t)CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V2 / CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V5)
-#define DYNAMIC_FEE_REFERENCE_TRANSACTION_WEIGHT        ((uint64_t)3)
+#define DYNAMIC_FEE_REFERENCE_TRANSACTION_WEIGHT        ((uint64_t)300000)
 
 #define ORPHANED_BLOCKS_MAX_COUNT                       100
 
